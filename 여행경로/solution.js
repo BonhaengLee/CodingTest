7cb93bc6d6a6f667de3250f89d302727cf25f58f
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2020-12-26 08:13:00 +0900

    ref. var -> const or let in 여행경로

--- 여행경로/solution.js
+++ 여행경로/solution.js
@@ -1,30 +1,33 @@
 // 테스트 케이스
-// const tickets = [
-//   ["ICN", "JFK"],
-//   ["HND", "IAD"],
-//   ["JFK", "HND"],
-// ];
 const tickets = [
-  ["ICN", "SFO"],
-  ["ICN", "ATL"],
-  ["SFO", "ATL"],
-  ["ATL", "ICN"],
-  ["ATL", "SFO"],
+  ["ICN", "JFK"],
+  ["HND", "IAD"],
+  ["JFK", "HND"],
 ];
+// const tickets = [
+//   ["ICN", "SFO"],
+//   ["ICN", "ATL"],
+//   ["SFO", "ATL"],
+//   ["ATL", "ICN"],
+//   ["ATL", "SFO"],
+// ];
 
-var answer = []; // resultList
+const answer = []; // resultList
 
 function solution(tickets) {
-  var result = new Array(tickets.length + 1);
-  var used = new Array(tickets.length + 1);
+  let result = new Array(tickets.length + 1);
+  let used = new Array(tickets.length + 1);
 
-  for (var i = 0; i < tickets.length; i++) {
+  for (let i = 0; i < tickets.length; i++) {
     // 최초 출발지는 ICN
     if (tickets[i][0] === "ICN") {
       // 최초 출발지마다 재귀함수 호출 수행
       // 첫 번째 티켓 정보는 미리 담아두고
+      console.log("rst0", tickets[i][0]);
+      console.log("rst1", tickets[i][1]);
       result[0] = tickets[i][0];
       result[1] = tickets[i][1];
+      console.log("출발", result);
       // 해당 티켓을 사용한 경우와 사용하지 않았을 경우를 모두 체크해줘야 함
       used[i] = true;
       dfs(tickets, used, 2, tickets[i][1], result);
@@ -33,7 +36,7 @@ function solution(tickets) {
   }
   // 배열 오름차순 정렬
   answer.sort(function (o1, o2) {
-    for (var i = 0; i < o1.length; i++) {
+    for (let i = 0; i < o1.length; i++) {
       if (o1[i].localeCompare(o2[i]) > 0) {
         return 1;
       } else if (o1[i].localeCompare(o2[i]) < 0) {
@@ -43,23 +46,25 @@ function solution(tickets) {
     return 0;
   });
   // 오름차순 정렬된 배열의 첫 번째 리턴
-  console.log(answer[0]);
+  console.log("리턴 전체", answer);
+  console.log("리턴", answer[0]);
   return answer[0];
 }
 
 function dfs(tickets, used, resultIdx, pre, result) {
   // 모든 티켓을 다 썼을 경우
   if (resultIdx === result.length) {
-    var tmp = new Array(result.length);
+    let tmp = new Array(result.length);
 
-    for (var i = 0; i < result.length; i++) {
+    for (let i = 0; i < result.length; i++) {
+      console.log("dfs for문, tmp할당", result[i]);
       tmp[i] = result[i];
     }
     answer.push(tmp);
     return;
   }
 
-  for (var i = 0; i < tickets.length; i++) {
+  for (let i = 0; i < tickets.length; i++) {
     // 이전 티켓의 목적지와 현재 티켓의 출발지가 같으면
     if (!used[i] && tickets[i][0] === pre) {
       result[resultIdx] = tickets[i][1]; // 도착지 입력
