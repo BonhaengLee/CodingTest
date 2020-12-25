cdb1f239566d46ace4dd18db882e6ca4771f8a74
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2020-12-26 00:52:01 +0900

    201226. 여행경로 완료2

--- 여행경로/solution.js
+++ 여행경로/solution.js
@@ -20,22 +20,17 @@ function solution(tickets) {
 
   for (var i = 0; i < tickets.length; i++) {
     // 최초 출발지는 ICN
-
     if (tickets[i][0] === "ICN") {
       // 최초 출발지마다 재귀함수 호출 수행
-
-      // 첫 번째 티켓 정보는 미리 담아두고 재귀함수 호출
+      // 첫 번째 티켓 정보는 미리 담아두고
       result[0] = tickets[i][0];
       result[1] = tickets[i][1];
-
-      // 재귀함수 호출
       // 해당 티켓을 사용한 경우와 사용하지 않았을 경우를 모두 체크해줘야 함
       used[i] = true;
       dfs(tickets, used, 2, tickets[i][1], result);
       used[i] = false;
     }
   }
-
   // 배열 오름차순 정렬
   answer.sort(function (o1, o2) {
     for (var i = 0; i < o1.length; i++) {
@@ -47,8 +42,7 @@ function solution(tickets) {
     }
     return 0;
   });
-
-  // 오름차순 정렬된 배열의 첫번 째 리턴
+  // 오름차순 정렬된 배열의 첫 번째 리턴
   console.log(answer[0]);
   return answer[0];
 }
@@ -61,7 +55,6 @@ function dfs(tickets, used, resultIdx, pre, result) {
     for (var i = 0; i < result.length; i++) {
       tmp[i] = result[i];
     }
-
     answer.push(tmp);
     return;
   }
@@ -70,7 +63,6 @@ function dfs(tickets, used, resultIdx, pre, result) {
     // 이전 티켓의 목적지와 현재 티켓의 출발지가 같으면
     if (!used[i] && tickets[i][0] === pre) {
       result[resultIdx] = tickets[i][1]; // 도착지 입력
-
       // 재귀함수 호출
       used[i] = true; // 티켓 사용
       dfs(tickets, used, resultIdx + 1, tickets[i][1], result);
