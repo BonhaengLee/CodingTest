7f37ffee2ab32178751840e016035c185bab6920
BonhaengLee <bon952014@ajou.ac.kr>
2021-06-30 23:50:27 +0900

    210630 pgrs-해시-1-완주하지못한선수 jest 세팅

---
+++ 해시-1-완주하지못한선수/solution.js
@@ -0,0 +1,11 @@
+// 테스트 케이스
+const files = ["leo", "kiki", "eden"]	
+const files2 = ["marina", "josipa", "nikola", "vinko", "filipa"]	
+const files3 = ["mislav", "stanko", "mislav", "ana"]
+
+function solution(participant, completion) {
+    var answer = '';
+    return answer;
+}
+
+console.log(solution(files));
