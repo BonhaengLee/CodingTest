c90a4d7b80d5608441e8d3991dd344c5975a39d2
BonhaengLee <bon952014@ajou.ac.kr>
2021-07-03 23:25:35 +0900

    210703 정렬-2-가장큰수

--- 정렬-2-가장큰수/test/test.js
+++ 정렬-2-가장큰수/test/test.js
@@ -1,26 +1,33 @@
-// 커맨드 배열의 요소만큼 for(반복)
-// 첫 요소를 받으면 array를 커맨드요소1,2로 slice > sort > 커맨드요소 3 + 1번째 요소를 리턴
+// @ : 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+// @ : 예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를
+// @ : 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+// @ : 0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는
+// @ : 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
 
-function solution(array, commands) {
-    var answer = [];
-    for(let i=0; i<commands.length; i++){
-        const arr = array.slice(commands[i][0]-1, commands[i][1]).sort((a,b)=>a-b)
-        
-        answer.push(arr[commands[i][2]-1]);
-    }
-    console.log(answer);
-    
-    return answer;
+// numbers의 요소를 for(반복)하면서 concat
+function solution(numbers) {
+    var answer = "";
+    console.log(numbers.map((num) => num.toString())); // 각 숫자를 이어주기 위해서 문자열로 변경
+
+    // console.log(
+    //     numbers
+    //         .map((num) => num.toString())
+    //         .sort((a, b) => {
+    //             console.log(b + a, a + b, b + a - (a + b));
+    //             return b + a - (a + b);
+    //         })
+    // );
+
+    answer = numbers
+        .map((num) => num.toString())
+        .sort((a, b) => b + a - (a + b));
+
+    // 모든 요소가 0인 경우에는 /^0+/를 이용해 판단하여 "0"만 리턴한다. reduce는 ""를 초깃값으로 요소를 차례대로 이어준다.
+    return answer.reduce((acc, curr) => (acc + curr).replace(/^0+/, "0"), "");
 }
 
 test("solution", () => {
-    expect(
-        solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])
-    ).toStrictEqual([5, 6, 3]);
-    // expect(
-    //     solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])
-    // ).toBe("vinko");
-    // expect(
-    //     solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
-    // ).toBe("mislav");
+    expect(solution([6, 10, 2])).toBe("6210");
+    expect(solution([3, 30, 34, 5, 9])).toBe("9534330");
+    // 배열 : toStrictEqual([5, 6, 3]);
 });
